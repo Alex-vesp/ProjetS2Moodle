e657a5881cf9a196c4e27ca5e13c9b4037bb0146
Ahmed <ahmed-el-hanafi.si-dehbi@alumni.univ-avignon.fr>
2022-02-03 18:19:28 +0100

    Add Teacher & ressources classes

---
+++ src/main/java/classes/Resources.java
@@ -0,0 +1,11 @@
+package classes;
+
+public abstract class Resources {
+    private String Name = "" ;
+    private String Description = "" ;
+
+    public Resources(String name, String description) {
+        this.Name = name;
+        this.Description = description;
+    }
+}
